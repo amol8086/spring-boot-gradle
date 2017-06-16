069104f5ac0ef57ccf8c5da3799e1b5c37bed1fd
Amol Kulkarni <amol8085@gmail.com>
2017-06-16 17:08:58 +0530

    first commit

---
+++ employee-mgmt/src/main/java/com/sgs/mgmt/EmpMgmtApplication.java
@@ -0,0 +1,12 @@
+package com.sgs.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpMgmtApplication.class, args);
+    }
+}
