c3b830a9c657ca902a7e55b55420a2034b71386a
Amol Kulkarni <amol8085@gmail.com>
2017-06-27 11:38:46 +0530

    Updated dependencies

--- employee-mgmt/src/main/java/com/sgs/mgmt/EmpMgmtApplication.java
+++ employee-mgmt/src/main/java/com/sgs/mgmt/EmpMgmtApplication.java
@@ -1,9 +1,13 @@
 package com.sgs.mgmt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class EmpMgmtApplication {
 
     public static void main(String[] args) {
